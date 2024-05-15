6fb3d3ee68d65b022980d0d30613355a8b716f76
mohammad <Mohdkn.15@gmail.com>
2024-05-15 21:29:03 +0300

    commit testing

---
+++ snake game/Snake.h
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+
+using namespace std;
+
+struct Point {
+	int x;
+	int y;
+	int index;
+
+};
+
+
+class Game {
+protected:
+	int size;
+	Point map[20][20];
+	int score;
+};
+class Snake
+{
+protected:
+	enum direction{up,down,left,right};
+	int length;
+	Point points[5];
+	string name;
+public:
+	
+
+
+	Snake(int length, string name)
+		: length(length), name(name)
+	{
+
+	}
+};
+
+
+class Coin {
+public:
+	int x;
+	int y;
+
+
+
+	Coin(){
+		x = rand()%100;
+		y = rand()%100;
+		cout << x << " " << y;
+	}
+};
--- snake game/snake game.cpp
+++ snake game/snake game.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <cstdlib> 
+#include <time.h>
 
 using namespace std;
 
 
+
 int main()
 {
+	int x = rand() % 100;
+	int y = rand() % 100;
+	srand(time(0));
 
+	cout << x << " " << y;
 
 }
